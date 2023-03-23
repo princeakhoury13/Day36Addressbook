d0abe5d8109adca3d455827c01c9b81e42fd91f8
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-23 14:20:40 +0530

    added: model class for address book

---
+++ AddressBookModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day36AddressBook
+{
+    public class AddressBookModel
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string name { get; set; }
+
+        public string contact_type { get; set; }
+    }
+}
