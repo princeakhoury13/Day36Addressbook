7e29ad3316fc030501f5e3b150f90a930165aab4
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-23 14:07:10 +0530

    added: Master branch greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day36AddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 36 assignment! ");
+        }
+    }
+}
